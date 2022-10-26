71d305ee9fe94cab2760af5d1f082acdb9cfe6ae
김동하 <dra700@neowiz.com>
2022-10-26 18:37:57 +0900

    debug test

--- cli/main.py
+++ cli/main.py
@@ -7,32 +7,34 @@ import configparser
 import jinja2
 import json
 import yaml
-from pathlib import Path
+#from pathlib import Path
 from atlassian import Jira
 
 class Config(object):
-    def __init__(self, *file_names):
+    def __init__(self, filename):
         parser = configparser.RawConfigParser()
         parser.optionxform = str
-        found = parser.read(file_names)
+        found = parser.read(filename)
         if not found:
             raise ValueError('No config file found!')
-        self.__dict__.update(parser.items(profile))
-    def update(self, *filenames, profile, url, username, password):
+        else:
+            self.__dict__.update(parser.items(profile))
+    def update(self, filename, profile, key, value):
         parser = configparser.RawConfigParser()
         parser.optionxform = str
-        if os.path.isfile(filenames):
-            parser.read_file(open(filenames))
-            parser.read(filenames)
+        if os.path.isfile(filename):
+            parser.read_file(open(filename))
+            parser.read(filename)
         if not parser.has_section(profile):
             parser.add_section(profile)
-        parser.set(profile, 'url', url)
-        parser.set(profile, 'username', username)
-        parser.set(profile, 'password', password)
-#todo url, username, password를 각각 받지 말고 함수 한번에 하나씩 총 3번 호출하는 방식으로 간단하게 변경할 것
+            parser.set(profile, key, value)
+            with open(filename, 'w') as f:
+                parser.write(f)
+                os.chmod(f, )
 
 profile = 'default'
-config_path = Path.home() / '.jira_cli' / 'credentials'
+config_path = os.path.join(os.path.expanduser('~'), '.jira_cli', 'credentials') # config path = ~/.jira_cli/credentials
+#config_path = Path.home() / '.jira_cli' / 'credentials'
 config = Config(config_path)
 jira = Jira(config.url, config.username, config.password)
 
@@ -52,17 +54,17 @@ def cli(**kwargs):
 @click.option('--url', default='https://jira.neowiz.com', prompt=True, help='Set Jira Site URL')
 @click.option('--username', prompt=True)
 @click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True)
-def configure(filenames, profile, url, username, password):
-    """create or modify .credentials for jira cli"""
-    filenames = config_path
-    Config.update(filenames, profile, url)
-    Config.update(filenames, profile, username)
-    Config.update(filenames, profile, password)
+def configure(profile, url, username, password):
+    '''create or modify ~/.jira_cli/credentials for jira cli'''
+    d = {'url': url, 'username': username, 'password': password}
+    for key, value in d.items():
+        result = Config.update(config_path, profile, key, value)
 
 #get issue
 @cli.command()
 @click.argument('key', type=str, required=True)
 def get_issue(key):
+    '''get issue from issue key'''
     try:
         result = jira.issue(key)
         click.echo(json.dumps(result))
@@ -74,6 +76,7 @@ def get_issue(key):
 @click.argument('key', type=str, required=True)
 @click.argument('field', type=str, required=True)
 def issue_field_value(key, field):
+    '''get issue field from issue key'''
     try:
         result = jira.issue_field_value(key, field)
         click.echo(json.dumps(result))
@@ -84,6 +87,7 @@ def issue_field_value(key, field):
 @cli.command()
 @click.pass_obj
 def update_issue_field(key, fields):
+    '''update issue field'''
     try:
         result = jira.update_issue_field(key, fields)
         click.echo(json.dumps(result))
@@ -95,9 +99,10 @@ def update_issue_field(key, fields):
 @click.argument('key', type=str, required=True)
 @click.option('--yes', '-y', is_flag=True, callback=abort_if_false, expose_value=False, prompt='Are you sure delete issue?')
 def delete_issue(key):
+    '''delete issue from issue key'''
     try:
         issue = jira.delete_issue(key)
-        click.echo(json.dumps(result))
+        click.echo(json.dumps(issue))
     except Exception as e:
         print(e)
 
@@ -109,6 +114,7 @@ def delete_issue(key):
 @click.option('--yaml', 'input_yaml', is_flag=True, show_default=True, default=True, help='create issue from yaml')
 @click.option('--json', 'input_json', is_flag=True, show_default=True, default=False, help='create issue from json')
 def create_issue(filename, duedate, project, input_yaml, input_json):
+    '''create issue from yaml template'''
     try:
         if input_yaml:
             with open('../../project.yaml', 'r') as yamlfile:
