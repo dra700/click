8345472658aa8ba396e77f75f6860cd874005718
김동하 <dra700@neowiz.com>
2022-10-28 16:01:37 +0900

    add configure

--- cli/main.py
+++ cli/main.py
@@ -2,53 +2,63 @@
 #coding=utf-8
 
 import os
+import sys
 import click
 import configparser
+import git
 import jinja2
 import json
 import yaml
-#from pathlib import Path
+import git
 from atlassian import Jira
 
 class Config(object):
-    def __init__(self, filename):
+    def __init__(self, profile, filename):
+        if not os.path.isfile(filename):
+            sys.exit("Config file not found. run 'jira-cli configure' first")
         parser = configparser.RawConfigParser()
         parser.optionxform = str
-        found = parser.read(filename)
-        try:
-            if not found:
-                raise ValueError("No config found. run 'jira-cli configure' first")
-            self.__dict__.update(parser.items(profile))
-        except ValueError or configparser.NoOptionError or configparser.NoOptionError as e:
-            print(e)
-    def update(self, filename, profile, key, value):
-        parser = configparser.RawConfigParser()
-        parser.optionxform = str
-        if os.path.isfile(filename):
-            parser.read_file(open(filename))
-            parser.read(filename)
+        parser.read(filename)
+        self.__dict__.update(parser.items(profile))
+
+#config 파일을 만들 때 사용하는 함수
+#Config class에 구현하려 했으나 __init__에서 config file이 없을 경우 더 이상 실행되지 않도록 처리해야 하기 때문에 configure를 실행할 수 없어 함수 분리함 
+def write_config(config_path, profile, k, v):
+    try:
+        parser = configparser.ConfigParser(interpolation=None)
+        splitted_path = os.path.split(config_path)
+        config_home = splitted_path[0]
+        if not os.path.exists(config_home):
+            os.makedirs(config_home)
+        if os.path.isfile(config_path):
+            parser.read_file(open(config_path))
+            parser.read(config_path)
         if not parser.has_section(profile):
             parser.add_section(profile)
-        parser.set(profile, key, value)
-        with open(filename, 'w') as f:
+        parser.set(profile, k, v)
+        with open(config_path, 'w') as f:
             parser.write(f)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
-profile = 'default'
-config_path = os.path.join(os.path.expanduser('~'), '.jira_cli', 'credentials') # config path = ~/.jira_cli/credentials
-try:
-    config = Config(config_path)
-    jira = Jira(config.url, config.username, config.password)
-except AttributeError as e:
-    print(e)
-
-#issue를 delete 할 때 사용할 실패 시 abort fuction
+#issue를 delete 할 때 사용할 실패 시 abort 함수
 def abort_if_false(ctx, param, value):
     if not value:
         ctx.abort()
 
+#project.yaml 파일을 찾기  위해 git repo 최상위 디렉토리를 찾아야 한다.
+def get_git_root(path):
+    repo = git.Repo(path, search_parent_directories=True)
+    result = repo.working_dir
+    return result
+
+config_path = os.path.join(os.path.expanduser('~'), '.jira_cli/', 'credentials') # config path = ~/.jira_cli/credentials
+
 # cli
 @click.group()
 def cli(**kwargs):
+    '''jira command line interface (cli)'''
     pass
 
 # configure
@@ -56,22 +66,31 @@ def cli(**kwargs):
 @click.option('--profile', default='default', help='profile name')
 @click.option('--url', default='https://jira.neowiz.com', prompt=True, help='Set Jira Site URL')
 @click.option('--username', prompt=True)
-@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True)
+@click.option('--password', prompt=True, hide_input=True)
 def configure(profile, url, username, password):
-    '''create or modify ~/.jira_cli/credentials for jira cli'''
-    config = Config(config_path)
+    '''create or modify config in ~/.jira_cli/credentials'''
     d = {'url': url, 'username': username, 'password': password}
     try:
-        for key, value in d.items():
-            config.update(config_path, profile, key, value)
+        for k, v in d.items():
+            write_config(config_path, profile, k, v)
     except Exception as e:
         print(e)
+    print("Config stored in {}\nSet profile[{}] complete!".format(config_path, profile))
 
 #get issue
 @cli.command()
+@click.option('--profile', default='default', help='profile name')
 @click.argument('key', type=str, required=True)
-def get_issue(key):
-    '''get issue from issue key'''
+def get_issue(profile, key):
+    '''get issue via issue key'''
+    config = Config(profile, config_path)
+    print(profile)
+    print(config_path)
+    print(config.url)
+    print(config.username)
+    print(config.password)
+    jira = Jira(config.url, config.username, config.password)
+    print(jira)
     try:
         result = jira.issue(key)
         click.echo(json.dumps(result))
@@ -80,10 +99,13 @@ def get_issue(key):
 
 #get issue field value
 @cli.command()
+@click.option('--profile', default='default', help='profile name')
 @click.argument('key', type=str, required=True)
 @click.argument('field', type=str, required=True)
-def issue_field_value(key, field):
-    '''get issue field from issue key'''
+def issue_field_value(profile, key, field):
+    '''get issue field via issue key'''
+    config = Config(profile, config_path)
+    jira = Jira(config.url, config.username, config.password)
     try:
         result = jira.issue_field_value(key, field)
         click.echo(json.dumps(result))
@@ -92,9 +114,12 @@ def issue_field_value(key, field):
 
 #update issue field
 @cli.command()
-@click.pass_obj
-def update_issue_field(key, fields):
+@click.option('--profile', default='default', help='profile name')
+@click.argument('key', type=str, required=True)
+def update_issue_field(profile, key, fields):
     '''update issue field'''
+    config = Config(profile, config_path)
+    jira = Jira(config.url, config.username, config.password)
     try:
         result = jira.update_issue_field(key, fields)
         click.echo(json.dumps(result))
@@ -103,10 +128,13 @@ def update_issue_field(key, fields):
 
 #delete issue
 @cli.command()
-@click.argument('key', type=str, required=True)
+@click.option('--profile', default='default', help='profile name')
 @click.option('--yes', '-y', is_flag=True, callback=abort_if_false, expose_value=False, prompt='Are you sure delete issue?')
-def delete_issue(key):
-    '''delete issue from issue key'''
+@click.argument('key', type=str, required=True)
+def delete_issue(profile, key):
+    '''delete issue via issue key'''
+    config = Config(profile, config_path)
+    jira = Jira(config.url, config.username, config.password)
     try:
         issue = jira.delete_issue(key)
         click.echo(json.dumps(issue))
@@ -115,16 +143,22 @@ def delete_issue(key):
 
 #create issue
 @cli.command()
-@click.argument('filename', type=click.Path(exists=True))
+@click.option('--profile', default='default', help='profile name')
 @click.option('--project', '-p', 'project', default='default', help='section in project.yaml')
-@click.option('--due-date', '-d', 'duedate', help="set due date when project needs ['YYYY-MM-DD']")
-@click.option('--yaml', 'input_yaml', is_flag=True, show_default=True, default=True, help='create issue from yaml')
-@click.option('--json', 'input_json', is_flag=True, show_default=True, default=False, help='create issue from json')
-def create_issue(filename, duedate, project, input_yaml, input_json):
-    '''create issue from yaml template'''
+@click.option('--due-date', '-d', 'duedate', help="set due date when project needs due date ['YYYY-MM-DD']")
+@click.option('--input-yaml', 'input_yaml', is_flag=True, show_default=True, default=True, help='create issue from yaml')
+@click.option('--input-json', 'input_json', is_flag=True, show_default=True, default=False, help='create issue from json')
+@click.option('--output-json', 'output_json', is_flag=True, show_default=True, default=False, help='when create issue from yaml, print json input to jira rest api')
+@click.argument('filename', type=click.Path(exists=True))
+def create_issue(profile, filename, duedate, project, input_yaml, input_json, output_json):
+    '''create issue from yaml template or json file'''
+    config = Config(profile, config_path)
+    jira = Jira(config.url, config.username, config.password)
     try:
+        git_root = get_git_root('.')
+        envfile = os.path.join(git_root, 'project.yaml')
         if input_yaml:
-            with open('../../project.yaml', 'r') as yamlfile:
+            with open(envfile, 'r') as yamlfile:
                 data = yaml.safe_load(yamlfile)
                 config = data[project]
                 watchers = list(config['watchers'])
@@ -133,10 +167,10 @@ def create_issue(filename, duedate, project, input_yaml, input_json):
                 template = jenv.get_template(filename)
                 rendered_template = template.render(config)
                 fields = yaml.safe_load(rendered_template)
-
                 if duedate is not None:
                     fields.update({'duedate': duedate})
-
+                if output_json:
+                    print(fields)
                 create_issue = jira.create_issue(fields)
                 click.echo(json.dumps(create_issue))
         if input_json:
@@ -145,6 +179,17 @@ def create_issue(filename, duedate, project, input_yaml, input_json):
     except Exception as e:
         print(e)
 
+
+# def template(envfile, template_file):
+#     with open(envfile, 'r') as yamlfile:
+#         data = yaml.safe_load(yamlfile)
+#         config = data[project]
+#         watchers = list(config['watchers'])
+#         loader = jinja2.FileSystemLoader(searchpath='./')
+#         jenv = jinja2.Environment(loader=loader)
+#         template = jenv.get_template(template_file)
+#         rendered_template = template.render(config)
+#         fields = yaml.safe_load(renderd_template)
 
 
 if __name__ == '__main__':
